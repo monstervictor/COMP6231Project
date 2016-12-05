450767c7e8aa60fce450c4802f13d5bffb2ff814
VictorF <monstervictor@gmail.com>
2016-12-04 21:11:52 -0500

    Added main to sequencer

---
+++ Common/src/communication/ReliableUDPReceiver.java
@@ -0,0 +1,19 @@
+package communication;
+
+import messages.IMessage;
+
+public class ReliableUDPReceiver implements IReliableUDPReceiver{
+
+	@Override
+	public IMessage receive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void notifySender(long seqID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Seq/src/main/MainSequencer.java
+++ Seq/src/main/MainSequencer.java
@@ -1,18 +1,29 @@
 package main;
 
-import UDPCommunication.SequencerSender;
-import data.Config;
-import data.ServerInfo;
+import java.io.IOException;
+
+import UDPCommunication.*;
+import communication.*;
+import data.*;
 
 public class MainSequencer {
 	
 	public static void main(String[] args){
+		Config.initialize();
 		
-		for (ServerInfo info : Config.getReplicas()) {
-			
+		IReliableUDPSender[] senders = new IReliableUDPSender[3];
+		for (int i = 0; i < Config.getReplicas().length; i++) {
+			try {
+				senders[i] = new ReliableUDPSender(Config.getReplicas()[i]);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		UDPReceiverImp receiver = new UDPReceiverImp(new ReliableUDPReceiver());
 		
-		
+		SequencerSender leSender = new SequencerSender(receiver, senders);
+		leSender.start();
 	}
 
 }
